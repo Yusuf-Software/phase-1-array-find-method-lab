6d71fd6d3b2e914881f4d8fa72383802d7161970
Yousif A. Dhahir <Yusuf.it.01@gmail.com>
2022-10-17 19:57:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(array) {
+    let res = "";
+    const result = array.find((item) => {
+      if (item.result == "W") {
+        res = item.year;
+        return res;
+      } else {
+        res = undefined;
+        return res;
+      }
+    });
+    return res;
+  }
+  
